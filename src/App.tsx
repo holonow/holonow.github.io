41bf2b998cf80a82936f3e701c8732a5f1402be1
wabilin <twelendil@protonmail.com>
2020-09-05 00:12:27 +0900

    Add NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,44 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
 import './App.css';
 import AppEffect from './components/AppEffect';
 import LiveTextList from './components/LiveTextList';
 import LiveFancyList from './components/LiveFancyList';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <RecoilRoot>
-      <AppEffect />
-      <div className="App">
-        <header className="App-header">
-          <p>
-            A Tool to Watch Hololive
-          </p>
-          <p>
-            (Coming soon)
-          </p>
-        </header>
-        <LiveFancyList />
-        <LiveTextList />
-      </div>
-    </RecoilRoot>
+    <Router>
+      <RecoilRoot>
+        <AppEffect />
+        <div className="App">
+          <NavBar />
+          <header className="App-header">
+            <h1>Holo Now</h1>
+            <p>
+              A Tool to Watch Hololive
+            </p>
+            <p>
+              (Coming soon)
+            </p>
+          </header>
+          <Switch>
+            <Route path="/text">
+              <LiveTextList />
+            </Route>
+            <Route path="/">
+              <LiveFancyList />
+            </Route>
+          </Switch>
+        </div>
+      </RecoilRoot>
+    </Router>
   );
 }
 
