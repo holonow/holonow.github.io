bbb103b157fa940300eeee006a8af75b1af37525
wabilin <wabilin@users.noreply.github.com>
2020-09-04 09:53:03 +0000

    deploy: febe708feb2c3eb1ac75bce750e9f9e6ca5dc1e8

---
+++ precache-manifest.862344db6163d3ec6d8a536469e35938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "affddc90f5733b5a9ff71105cf82edc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67ae63b11d6456a6da22",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "023510032b1d10044229",
+    "url": "/static/js/2.ede47ea9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ede47ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ae63b11d6456a6da22",
+    "url": "/static/js/main.08867fe7.chunk.js"
+  },
+  {
+    "revision": "d42ab44c7776e8628701",
+    "url": "/static/js/runtime-main.2e05ccb0.js"
+  }
+]);
