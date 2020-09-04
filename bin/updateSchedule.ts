f9ea9bfc294c9a3ab8cf68a1f97de57df2e01064
wabilin <twelendil@protonmail.com>
2020-09-04 19:05:40 +0900

    use existed map

--- bin/updateSchedule.ts
+++ bin/updateSchedule.ts
@@ -1,6 +1,9 @@
 import parseScheduleHtml from 'holo-schedule';
 import getScheduleHtml from 'holo-schedule/lib/getScheduleHtml';
 import { writeFile } from 'fs';
+import axios from 'axios'
+
+const MAP_URL = 'https://holonow.github.io/data/imageMap.json'
 
 function write(path: string, data: string): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -15,8 +18,10 @@ function write(path: string, data: string): Promise<void> {
 }
 
 async function main() {
+  const { data } = await axios.get(MAP_URL)
+
   const html = await getScheduleHtml();
-  const { lives, dict } = parseScheduleHtml(html);
+  const { lives, dict } = parseScheduleHtml(html, data);
 
   return Promise.all([
     write('public/data/schedule.json', JSON.stringify(lives)),
