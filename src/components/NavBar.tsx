bfd1a953ebe6ec4e1c9433370d6cb4f391bed218
wabilin <twelendil@protonmail.com>
2020-09-06 13:57:41 +0900

    Add GitHub link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,27 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 const Nav = styled.nav`
   padding-left: clamp(.5rem, 2vw, 1rem);
   padding-right: clamp(.5rem, 2vw, 1rem);
   background-color: #333;
-`;
-
-const Ul = styled.ul`
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-`;
-
-const Li = styled.li`
-  float: left;
 
   a {
-  display: block;
-  padding: 5px;
-  color: white;
-  text-decoration: none;
+    display: block;
+    padding: 5px;
+    color: white;
+    text-decoration: none;
   }
 
   a:hover {
@@ -30,22 +22,58 @@ const Li = styled.li`
   }
 `;
 
+const LeftUl = styled.ul`
+  float: left;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+
+  li {
+    float: left;
+  }
+`;
+
+const RightUl = styled(LeftUl)`
+  float: none;
+  li {
+    float: right;
+  }
+`;
+
+const GitHubLink = () => (
+  // eslint-disable-next-line react/jsx-no-target-blank
+  <a target="_blank" href="https://github.com/holonow/holonow.github.io">
+    <FontAwesomeIcon icon={faGithub} />
+  </a>
+);
+
+const JsonLink = () => (
+  // eslint-disable-next-line react/jsx-no-target-blank
+  <a target="_blank" href="https://holonow.github.io/holo-data/schedule.json">
+    JSON
+  </a>
+);
+
 function NavBar() {
   return (
     <Nav>
-      <Ul>
-        <Li>
-          <Link style={{ fontWeight: 'bold' }} to="/">[Holo now]</Link>
-        </Li>
-        <Li>
+      <LeftUl>
+        <li>
+          <Link style={{ fontWeight: 'bold' }} to="/">[Holo Now]</Link>
+        </li>
+        <li>
           <Link to="/text">Text</Link>
-        </Li>
-        <Li>
-          <a target="_blank" rel="noreferrer noopener" href="https://holonow.github.io/holo-data/schedule.json">
-            JSON
-          </a>
-        </Li>
-      </Ul>
+        </li>
+        <li>
+          <JsonLink />
+        </li>
+      </LeftUl>
+      <RightUl>
+        <li>
+          <GitHubLink />
+        </li>
+      </RightUl>
     </Nav>
   );
 }
