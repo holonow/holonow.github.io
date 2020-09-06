07caf34455b33883da5b7151df965e7a0cd7ae21
wabilin <twelendil@protonmail.com>
2020-09-06 16:44:43 +0900

    fix default state

--- src/store/ui.ts
+++ src/store/ui.ts
@@ -2,7 +2,7 @@ import { atom } from 'recoil';
 
 type ShowSettingsState = boolean
 
-const defaultState: ShowSettingsState = true;
+const defaultState: ShowSettingsState = false;
 export const showSettingsState = atom({
   key: 'ui/showSettings',
   default: defaultState,
