a1f58c4cefb7daa45fe7aabbf2cdd1ca729cd30d
wabilin <wabilin@users.noreply.github.com>
2020-09-11 12:46:47 +0000

    deploy: c407fa4c33fc084d90a0c68173e8a6bc93d93e8f

--- precache-manifest.8516563dc8f91d4eb38614c45682a2ae.js
+++ precache-manifest.8516563dc8f91d4eb38614c45682a2ae.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "688d7851b5748dd58f588b6af67e222e",
+    "revision": "cc9b6fd1187b6aa8b551eb57f78e5a29",
     "url": "/index.html"
   },
   {
-    "revision": "352edbfb6df014ff5287",
+    "revision": "83706238b2e49f5c17f3",
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.3526b958.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "352edbfb6df014ff5287",
-    "url": "/static/js/main.67cd1eb1.chunk.js"
+    "revision": "83706238b2e49f5c17f3",
+    "url": "/static/js/main.c26b324b.chunk.js"
   },
   {
     "revision": "d42ab44c7776e8628701",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a1dfdf5d38d39d84e747b2a83976c763.js"
+  "/precache-manifest.8516563dc8f91d4eb38614c45682a2ae.js"
 );
 
 self.addEventListener('message', (event) => {
