8ac147842ef6acc42ac05a5a242b16daf45bacec
Ting Shu Lin <2549826+wabilin@users.noreply.github.com>
2021-12-09 21:43:24 +0900

    feat: improve some performance issue (#12)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,6 +25,7 @@ function App({ Component, pageProps }: AppProps) {
         />
         <link rel="apple-touch-icon" href="/logo192.png" />
         <link rel="manifest" href="/manifest.json" />
+        <link rel="preconnect" href="https://img.youtube.com" />
         <title>Holo Now</title>
       </Head>
       <GlobalStyle />
